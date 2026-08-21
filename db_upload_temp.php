3d4e25fe9eecdb0725ce2499dfcc14a5726c4866
kamchalauuu-ai <kamchalauuu@gmail.com>
2026-08-21 17:21:52 +0530

    Remove temp uploader

--- db_upload_temp.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['dbfile'])) {
-    if ($_POST['secret'] !== 'bfs-secret-123') die('Unauthorized');
-    if (move_uploaded_file($_FILES['dbfile']['tmp_name'], __DIR__ . '/crm.db')) {
-        echo "UPLOAD_SUCCESS";
-    } else {
-        echo "UPLOAD_FAILED";
-    }
-}
